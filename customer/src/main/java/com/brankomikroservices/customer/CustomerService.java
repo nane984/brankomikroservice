98c514d9b512fd98966406d09629a4cc9f7c4a53
nane984 <br.scekic@gmail.com>
2024-01-26 13:55:16 +0100

    Sync communication between customer and fraud

--- customer/src/main/java/com/brankomikroservices/customer/CustomerService.java
+++ customer/src/main/java/com/brankomikroservices/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.brankomikroservices.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
@@ -16,6 +18,20 @@ public class CustomerService {
                 .build();
         //todo: check if email valid
         //todo: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);  //da bi sacekao da sacuva i da bi onda imali id
+
+        //todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
+
+
+        //todo: send notification
     }
 }
---
+++ customer/src/main/java/com/brankomikroservices/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.brankomikroservices.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
