c2fe455b7068eb3d7e5718d4b52bd62186fadfe9
branko <br.scekic@gmail.com>
2024-02-09 00:22:10 +0100

    dockerized - correct url

--- notification/src/main/java/com/brankomicroservices/notification/NotificationApplication.java
+++ notification/src/main/java/com/brankomicroservices/notification/NotificationApplication.java
@@ -1,10 +1,9 @@
 package com.brankomicroservices.notification;
 
-import com.brankomicroservices.amqp.RabbitMQMessageProducer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
+
 
 @SpringBootApplication(
         scanBasePackages = {
